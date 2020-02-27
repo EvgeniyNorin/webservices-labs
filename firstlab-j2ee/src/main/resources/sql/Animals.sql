c03d365b57dcf0a2c203a57fa74e266a2cd4064c
Evgeniy <evgeniy.norin@gmail.com>
2020-02-28 01:27:16 +0300

    delete unnecessary ddl, add reports

--- firstlab-j2ee/src/main/resources/sql/Animals.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE ARTICLE
-(
-  article_id   bigserial primary key,
-  author_id    varchar NOT NULL,
-  h_index      BIGINT  NOT NULL,
-  article_name varchar NOT NULL,
-  article_desc varchar NOT NULL,
-  date_added   BIGINT  NOT NULL
-);
