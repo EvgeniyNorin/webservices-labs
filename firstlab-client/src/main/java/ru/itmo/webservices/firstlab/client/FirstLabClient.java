740c18138a1d802e2821b618024d04a43d2ca198
e.norin <e.norin@macbook-e.norin>
2020-06-10 21:26:43 +0300

    add completablefuture usage

--- firstlab-client/src/main/java/ru/itmo/webservices/firstlab/client/FirstLabClient.java
+++ firstlab-client/src/main/java/ru/itmo/webservices/firstlab/client/FirstLabClient.java
@@ -7,26 +7,46 @@ import ru.itmo.webservices.firstlab.standalone.ArticlesService;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FirstLabClient {
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static ArticleWebService createService() throws MalformedURLException {
         URL url = new URL("http://localhost:8080/ArticleService?wsdl");
         ArticlesService articlesService = new ArticlesService(url);
-        ArticleWebService articlesWebService = articlesService.getArticleWebServicePort();
+        return articlesService.getArticleWebServicePort();
+    }
 
-
+    public static ArticleWebService runFirstExample(ArticleWebService articlesWebService) {
         System.out.println("Querying all existing Articles");
         List<Article> articles = articlesWebService.getArticles(null, null, null, null, null);
         for (Article picture : articles) {
             System.out.println(picture.toString());
         }
+        return articlesWebService;
+    }
 
+
+    public static ArticleWebService runSecondExample(ArticleWebService articlesWebService) {
         System.out.println("Querying all Articles by specific fields");
         List<Article> articles2 = articlesWebService.getArticles(null, 15L, null, "article_desc_1", null);
         for (Article picture : articles2) {
             System.out.println(picture.toString());
         }
+        return articlesWebService;
+    }
 
+    public static void main(String[] args) throws MalformedURLException, ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        // API calls are still synchronous because of the nature of wsimport autogen code, but their composition is asynchronous
+        // Finally we call get() method - this call obviously synchronous, but it serves as a point of synchronization
+        CompletableFuture
+                .completedFuture(createService())
+                .thenComposeAsync(service -> CompletableFuture.supplyAsync(() -> runFirstExample(service)), executorService)
+                .thenComposeAsync(service -> CompletableFuture.supplyAsync(() -> runSecondExample(service)), executorService)
+                .get();
     }
 }
